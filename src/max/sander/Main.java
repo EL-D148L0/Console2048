c8426fdd71010248c5dade89a04505847532a620
Max <max_sander@outlook.de>
2022-06-12 17:30:39 +0200

    library setup

--- src/max/sander/Main.java
+++ src/max/sander/Main.java
@@ -1,16 +1,29 @@
 package max.sander;
 
+import io.webfolder.curses4j.Window;
+
+import static io.webfolder.curses4j.Curses.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        /*
         Printer printer = new Printer();
         Board board = new Board(2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 0, 0);
         printer.drawGame(board);
         printer.screen.print();
 //        System.out.println();
+        */
 
+        initscr();
+
+        Window win1 = newwin(9, 9, 1, 1);
+
+
+        getch();
+
+        endwin();
     }
 }
 
